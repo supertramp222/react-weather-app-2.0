43c185563f77d464bd166870be3c8d28e67599a0
supertramp222 <38687331+supertramp222@users.noreply.github.com>
2022-08-16 19:32:01 +0200

    Added the GitHub repo URL

--- src/App.js
+++ src/App.js
@@ -1,5 +1,19 @@
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <footer>
+        {" "}
+        This project is created by Katharina Ramer. It is open-sourced. {""}
+        <a
+          href="https://github.com/supertramp222/react-weather-app-2.0"
+          target="_blanl"
+        >
+          View on GiHub
+        </a>
+      </footer>
+    </div>
+  );
 }
