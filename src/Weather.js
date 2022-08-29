a8ab3ddf56b009906276cf1937c93195468d5ca8
supertramp222 <38687331+supertramp222@users.noreply.github.com>
2022-08-29 15:05:32 +0200

    Added basic html and css structure for forecast

--- src/Weather.js
+++ src/Weather.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import axios from "axios";
+import WeatherForecast from "./WeatherForecast";
 import WeatherInfo from "./WeatherInfo";
 import "./Weather.css";
 
@@ -61,6 +62,7 @@ export default function Weather(props) {
           </div>
         </form>
         <WeatherInfo data={weatherData} />
+        <WeatherForecast />
       </div>
     );
   } else {
